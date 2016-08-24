6370ed7eb735fd085d3a520ba4fcb74fbccc6b00
Emanuele Firmani <emanuele.firmani@outlook.com>
2016-08-24 22:48:45 +0200

    DvorakProgrammer: numbers are shifted one row up

--- keyboards/ergodox/keymaps/dvorak_programmer/keymap.c
+++ keyboards/ergodox/keymaps/dvorak_programmer/keymap.c
@@ -61,15 +61,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 1: Symbol Layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |        |      |      |      |      |      |      |           |      |      |   7  |   8  |   9  |   *  |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   !  |   @  |   $  |   #  |      |      |           |      |      |   7  |   8  |   9  |   *  |        |
+ * |        |   !  |   @  |   $  |   #  |      |      |           |      |      |   4  |   5  |   6  |   +  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        | RGui | RAlt | RShft| RCtrl|   |  |------|           |------|      |   4  |   5  |   6  |   +  |        |
+ * |        | RGui | RAlt | RShft| RCtrl|   |  |------|           |------|      |   1  |   2  |   3  |   \  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   %  |   ^  |   ~  |   `  |      |      |           |      |      |   1  |   2  |   3  |   \  |        |
+ * |        |   %  |   ^  |   ~  |   `  |      |      |           |      |      |   0  |    . |   =  |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |   0  |    . |   =  |      |      |
+ *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
@@ -91,11 +91,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 																				KC_TRNS,
 														KC_TRNS,	KC_TRNS,	KC_TRNS,
 		// right hand
-		KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,
 		KC_TRNS,	KC_TRNS,	KC_7,		KC_8,		KC_9,		KC_ASTR,	KC_TRNS,
-					KC_TRNS,	KC_4,		KC_5,		KC_6,		KC_PLUS,	KC_TRNS,
-		KC_TRNS,	KC_TRNS,	KC_1,		KC_2,		KC_3,		KC_BSLS,	KC_TRNS,
-								KC_0,		KC_DOT,		KC_EQL,		KC_TRNS,	KC_TRNS,
+		KC_TRNS,	KC_TRNS,	KC_4,		KC_5,		KC_6,		KC_PLUS,	KC_TRNS,
+					KC_TRNS,	KC_1,		KC_2,		KC_3,		KC_BSLS,	KC_TRNS,
+		KC_TRNS,	KC_TRNS,	KC_0,		KC_DOT,		KC_EQL,		KC_TRNS,	KC_TRNS,
+								KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,
 
 		KC_TRNS,	KC_TRNS,
 		KC_TRNS,
